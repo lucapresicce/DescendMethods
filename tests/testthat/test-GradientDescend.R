93b43b59993ce6fa81371bc05811e5f1644bb15e
lucapresicce <l.presicce@campus.unimib.it>
2022-04-05 16:50:21 +0200

    tests

--- tests/testthat/test-GradientDescend.R
+++ tests/testthat/test-GradientDescend.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("simulated data", {
+  set.seed(1)
+  n <- 100
+  x <- cbind(1, matrix(runif(n*3, 0, 10), nrow = n, ncol = 3))
+  beta <- rnorm(4, 5, 5)
+  y <- x %*% beta + rnorm(n)
+  df <- list(X = x, Y = y)
+  res <- gd(data = df, verb = F, maxit = 3000)
+  expect_equal(as.numeric(res$Beta_hat), beta, tolerance = 1)
+})
+
+test_that("real data", {
+  set.seed(1)
+  n <- 100
+  x <- cbind(1, matrix(runif(n*3, 0, 10), nrow = n, ncol = 3))
+  beta <- rnorm(4, 5, 5)
+  y <- x %*% beta + rnorm(n)
+  df <- list(X = x, Y = y)
+  res <- gd(data = df, verb = F, maxit = 3000)
+  expect_equal(as.numeric(res$Beta_hat), beta, tolerance = 1)
 })
