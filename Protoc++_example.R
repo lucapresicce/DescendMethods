0b29b44584b3439db48afccda2187d08228649c4
Alessandro Colombi <colombi.alessandro@hotmail.it>
2022-07-08 17:33:36 +0200

    sampler implementation

--- Protoc++_example.R
+++ Protoc++_example.R
@@ -8,3 +8,38 @@ ls("RProtoBuf:DescriptorPool")
 # Read message in R
 writeLines(as.character(RProtoBuf::read(MyNamespace.MySave, out_R)))
 
+
+
+# C++ example in Bayesian statistics
+
+# Generate data
+mu_true = c(0,1)
+Sigma_true = matrix(c(1,0.5,0.5,1),nrow =2,byrow=T)
+set.seed(12412)
+data = mvtnorm::rmvnorm(n=150,mean=mu_true,sigma = Sigma_true)
+
+# Set hyperparameters
+mu0 = c(0,0)
+Lambda0 = diag(2)
+b = 3
+D = diag(2)
+
+# Set initial values
+mu_init = c(0,0)
+Sigma_init = diag(2)
+
+# Set number of iterations
+niter = 1000
+burnin = 1
+
+res = BayesLM(data,niter,burnin,mu0,Lambda0,b,D,mu_init,Sigma_init)
+
+
+
+
+
+
+
+
+
+
--- src/Proto_in_cplusplus.cpp
+++ src/Proto_in_cplusplus.cpp
@@ -1,5 +1,4 @@
 // [[Rcpp::depends(RcppEigen)]]
-// [[Rcpp::depends(RcppParallel)]]
 #define STRICT_R_HEADERS
 #include <Rcpp.h>
 #include <RcppEigen.h>
@@ -7,6 +6,7 @@
 #include "MySave.pb.h"
 #include "GSL_wrappers.h"
 #include "Rcpp.h"
+#include <Eigen/LU>
 
 
 // This is a simple example of exporting a C++ function to R. You can
@@ -54,7 +54,42 @@ Rcpp::RawVector Proto_in_cplusplus() {
 }
 
 
+Rcpp::RawVector BayesLM(Eigen::MatrixXd const & data, unsigned int const & niter, unsigned int const & burnin,
+                        double const & mu0, Eigen::MatrixXd const & Lambda0,double const & b,Eigen::MatrixXd const & D,
+                        Eigen::MatrixXd const & mu_init, Eigen::MatrixXd const & Sigma_init){
 
+  Rcpp::RawVector out;
+    unsigned int p = mu_init.size();
+  unsigned int n = data.rows();
+  sample::rmvnorm rmv;
+  sample::rwish   rw;
+
+  Eigen::VectorXd ones(Eigen::VectorXd::Constant(p,1.0));
+  Eigen::VectorXd Id(Eigen::MatrixXd::Constant(p,p,1.0));
+  
+  Eigen::VectorXd mu(mu_init);
+  Eigen::MatrixXd Sigma(Sigma_init);
+  Eigen::MatrixXd K(Sigma_init.partialPivLu().solve(Id));
+  
+  Eigen::VectorXd invLambda0mu0(Lambda0.partialPivLu().solve(mu0));
+  Eigen::MatrixXd invLambda0(Lambda0.partialPivLu().solve(Id));
+  Eigen::VectorXd data_mean(data.colwise().sum()/n);
+
+  // Start MCMC
+  for(std::size_t i = 0; i < niter; i++){
+    Eigen::MatrixXd Lambda_n = (invLambda0 + n*K).partialPivLu().solve(Id) ;
+    Eigen::VectorXd mu_n = Lambda_n * (invLambda0mu0 + n*K) ;
+    mu = rmv(mu_n,Lambda_n);
+    Rcpp::Rcout<<"mu: "<<std::endl<<mu<<std::endl;
+    Eigen::MatrixXd U(Eigen::MatrixXd::Constant(p,p,0.0));
+    for(std::size_t j = 1; j<n; j++){
+      U += (data.row(j)-mu).transpose()*(data.row(j)-mu);
+    }
+    K = rw(b+n,D+U);
+    Rcpp::Rcout<<"K: "<<std::endl<<K<<std::endl;
+  }
+  return out;
+}
 
 
 
